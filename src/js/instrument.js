316457402b91b08dd2d658c37696a7e0aa2ac785
julia-martynovych <inquam414@gmail.com>
2025-01-30 13:51:32 +0100

    burger menu

--- src/js/instrument.js
+++ src/js/instrument.js
@@ -1,1 +1,7 @@
-// Aqui va el codigo del instrumento
+document.getElementById("burger-menu").addEventListener("click", function () {
+    let navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("active");
+
+
+    this.classList.toggle("active");
+});
