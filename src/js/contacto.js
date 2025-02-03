7a5081b550218a0213e41fa84118b3b2ebe6da67
Noemi1977 <noemi.casaprima.pendas@gmail.com>
2025-02-03 22:24:16 +0100

    valids form

--- src/js/contacto.js
+++ src/js/contacto.js
@@ -1,5 +1,23 @@
+//validaciones de formulario
+
+// EMAIL
+const email=document.getElementById("email")
+
+email.addEventListener("input", function () {
+  const basicEstructure = /^[^\s@]+@[^\s@]+\.[^\s@]+$/  //comprobamos que el email tiene una estructura básica 
+  if (!basicEstructure.test(this.value)) { //comprobamos si el valor que se introduce no es válido y si no lo es se lo mostramos 
+    this.setCustomValidity("Por favor, ingresa un correo válido.");
+  } else {
+    this.setCustomValidity("");
+  }
+});
+
+
+
+// BOTON ENVIAR
+
 const form=document.querySelector('form')
-const okMens= document.getElementById('--ok-mesaje')
+const okMens= document.getElementById('ok-mesaje')
 
 form.addEventListener('submit', function(event) {
     event.preventDefault(); 
@@ -7,7 +25,7 @@ form.addEventListener('submit', function(event) {
     //El mensaje esta oculto esto lo muestra 
     okMens.style.display = 'block';
 
-   //El mensaje desaparece después de30 segundos
+   //El mensaje desaparece después de 30 segundos
     setTimeout(function() {
         okMens.style.display = 'none';
     }, 30000); 
