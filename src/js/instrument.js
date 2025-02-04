e8afb497b754b8b21c26980b75d15c529a0c7d45
Coral1331 <konata1331@gmail.com>
2025-02-04 14:37:48 +0100

    changes

--- src/js/instrument.js
+++ src/js/instrument.js
@@ -1,1 +1,35 @@
 // Aqui va el codigo del instrumento
+// Función parámetro instrumento - URL
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+  }
+
+  // Obtener el parámetro 'instrumento'
+  const instrumentoParam = getQueryParam('instrumento');
+  if (instrumentoParam) {
+    // Cambios de título (h1) con el nombre del instrumento
+    const titulo = document.querySelector('.construccion h1');
+    titulo.textContent = decodeURIComponent(instrumentoParam);
+    
+    // Actualizar el atributo alt de la imagen
+    const imagen = document.querySelector('.ilustracion img');
+    imagen.alt = `${decodeURIComponent(instrumentoParam)} en construcción`;
+    
+    // SRC Cambio de ilustración según el instrumento
+    switch (instrumentoParam.toLowerCase()) {
+      case 'guitarra':
+        imagen.src = "../public/image/PH-GUITARRA.png";
+        break;
+      case 'acordeon':
+        imagen.src = "../public/image/PH-ACORDEON.png";
+        break;
+      case 'tambor':
+        imagen.src = "../public/image/PH-TAMBOR.png";
+        break;
+      default:
+        // Asignar una imagen por defecto ¿?
+        imagen.src = "../public/image/PLACEHOLDER-default.png";
+        break;
+    }
+  }
